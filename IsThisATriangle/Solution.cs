4a5a65e6759ff4f08399b96a37a25d9cbcdcc111
devalexer <alliemckay.dev@gmail.com>
2017-07-09 13:43:53 -0400

    SmallestInInArray

---
+++ IsThisATriangle/Solution.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace IsThisATriangle
+{
+
+    public class Triangle2
+    {
+        public static bool IsATriangle(int a, int b, int c)
+        {
+            var max = new int[] { a, b, c }.Max();
+            var sum = a + b + c;
+            if (sum - max > max)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+
+//Implement a method that accepts 3 integer values a, b, c. The method should return true if a triangle can be built with the sides of given length and false in any other case.
+//(In this case, all triangles must have surface greater than 0 to be accepted).
+
+    //public class Triangle
+    //{
+    //    public static bool IsTriangle(int a, int b, int c)
+    //    {
+    //        while (a * b * c >= 0)
+    //        {
+    //            if (Math.Pow(a, 2) + Math.Pow(b, 2) >= Math.Pow(c, 2))
+    //            {
+    //                return true;
+    //            }
+    //            else
+    //            {
+    //                return false;
+    //            }
+    //        }
+    //    }
+    //}
+}
---
+++ SmallestIntInArray/Solution.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SmallestIntInArray
+{
+    public class Kata
+    {
+        public static int FindSmallestInt(int[] args)
+        {
+            return args.Min();
+        }
+    }
+
+    //Given an array of integers your solution should find the smallest integer.
+    //For example:
+
+    //    Given[34, 15, 88, 2] your solution will return 2
+    //    Given[34, -345, -1, 100] your solution will return -345
+
+    //You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+}
