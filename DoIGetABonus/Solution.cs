e4c57e33d2b1b6876bb721ad11195bf35159d4f9
devalexer <alliemckay.dev@gmail.com>
2017-07-11 18:52:31 -0400

    DoIGetABonus Refactored

--- DoIGetABonus/Solution.cs
+++ DoIGetABonus/Solution.cs
@@ -10,7 +10,7 @@ namespace DoIGetABonus
         public static string bonus_time(int salary, bool bonus)
         {
             var final = 0;
-            if (bonus == true)
+            if (bonus)
             {
                 final = salary * 10;
             }
@@ -21,10 +21,14 @@ namespace DoIGetABonus
             return ("$" + final);
         }
 
-//It's bonus time in the big city! The fatcats are rubbing their paws in anticipation... but who is going to make the most money?
-//Build a function that takes in two arguments(salary, bonus). Salary will be an integer, and bonus a boolean.
-//If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
-//Return the total figure the individual will receive as a string prefixed with '£' (JS and Java) or '$' (C#, C++, Ruby, Clojure, Elixir, PHP and Python).
+        //It's bonus time in the big city! The fatcats are rubbing their paws in anticipation... but who is going to make the most money?
+        //Build a function that takes in two arguments(salary, bonus). Salary will be an integer, and bonus a boolean.
+        //If bonus is true, the salary should be multiplied by 10. If bonus is false, the fatcat did not make enough money and must receive only his stated salary.
+        //Return the total figure the individual will receive as a string prefixed with '£' (JS and Java) or '$' (C#, C++, Ruby, Clojure, Elixir, PHP and Python).
 
+
+
+
+        public static string bonus_time2(int salary, bool bonus) => "$" + (bonus ? salary * 10 : salary);
     }
 }
