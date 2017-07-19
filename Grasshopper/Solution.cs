168f74642accddf127567b6855fe801f1c5fc3d3
devalexer <alliemckay.dev@gmail.com>
2017-07-18 21:54:46 -0400

    Grasshopper

---
+++ Grasshopper/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Grasshopper
+{
+    public class Kata
+    {
+        public static int AddFive(int num)
+        {
+            return (num + 5);
+        }
+    }
+}
