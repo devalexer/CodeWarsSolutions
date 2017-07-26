44978bf1c8cbf97b1f8f49d1d5e36f8570e420b2
devalexer <alliemckay.dev@gmail.com>
2017-07-25 20:23:22 -0400

    LostNumInSequence

---
+++ AreWeAlternate/Solution.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace AreWeAlternate
+{
+    public class Kata
+    {
+        public static bool IsAlt(string word)
+        {
+            string[] s = string.Join<char>("", word.Where((ch, index) => (index % 2) != 0));
+            if (s == ("a" || "e" || "i" || "o" || "u"))
+            {
+                return true;
+            }
+            if (s != ("a" || "e" || "i" || "o" || "u"))
+            {
+                return false;
+            }
+
+
+            StringBuilder sb = new StringBuilder();
+                for (int i = 0; i <= word.Length - 1; i++)
+                {
+                    sb.AppendFormat(i % 2 != 0 ? "{0} " : "{0},", [i]);
+                }
+                return sb.ToString();
+            }
+        }
+    }
+
+
+//Create a function isAlt() that accepts a string as an argument and validates whether the vowels(a, e, i, o, u) and consonants are in alternate order.
+
+//isAlt("amazon")
+//// true
+//isAlt("apple")
+//// false
+//isAlt("banana")
+//// true
+
+//Arguments consist of only lowercase letters.
+}
---
+++ LostNumInSequence/Solution.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LostNumInSequence
+{
+    public class Kata
+    {
+        public static int FindDeletedNumber(List<int> startingList, List<int> mixedList) =>
+
+            startingList.Except(mixedList).FirstOrDefault();
+
+        //startingList.Sort() .Delete(n => n.Equals(;
+        //mixedList.Sort();
+
+        //return (startingList.Sort() - mixedList.Sort());
+
+        //var startTotal = startingList.Sum();
+        //var mixTotal = mixedList.Sum();
+
+        //    return startTotal - mixTotal;
+
+        }
+    }
+
+
+
+    //An ordered sequence of numbers from 1 to N is given. One number might have deleted from it, then the remaining numbers were mixed.
+    //Find the number that was deleted.
+
+    //Example:
+
+    //   The starting array sequence is [1,2,3,4,5,6,7,8,9]
+    //    The mixed array with one deleted number is [3,2,4,6,7,8,1,9]
+    //    Your function should return the int 5.
+
+    //If no number was deleted from the array and no difference with it, your function should return the int 0.
+    //Note that N may be 1 or less (in the latter case, the first array will be[]).
+
+}
